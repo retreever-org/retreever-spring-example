4f7d9a04592c9e3c910caf015e426ea527d3ee06
Raju Gowda <rajugowda0232@gmail.com>
2026-06-04 08:39:42 +0530

    added retreever jar

--- src/test/java/dev/retreever/example/security/SecurityCorsIntegrationTest.java
+++ src/test/java/dev/retreever/example/security/SecurityCorsIntegrationTest.java
@@ -14,25 +14,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SecurityCorsIntegrationTest {
 
-    private static final String DEV_ORIGIN = "http://localhost:5173";
     private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
 
     @LocalServerPort
     private int port;
 
     @Test
-    void retreeverPingIncludesCorsHeadersForPublicDevRequests() throws Exception {
+    void retreeverPingIsPublicForSameOriginRequests() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:" + port + "/retreever/ping"))
-                .header("Origin", DEV_ORIGIN)
                 .GET()
                 .build();
 
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(200, response.statusCode());
-        assertEquals(DEV_ORIGIN, response.headers().firstValue("Access-Control-Allow-Origin").orElse(null));
-        assertEquals("true", response.headers().firstValue("Access-Control-Allow-Credentials").orElse(null));
     }
 
     @Test
